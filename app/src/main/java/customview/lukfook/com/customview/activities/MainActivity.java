83c869c5a59127a69387410972b7217671e06e55
sky <macan1209@vip.qq.com>
2016-01-26 14:52:34 +0800

    添加橡皮擦代码

--- app/src/main/java/customview/lukfook/com/customview/activities/MainActivity.java
+++ app/src/main/java/customview/lukfook/com/customview/activities/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import customview.lukfook.com.customview.R;
-import customview.lukfook.com.customview.views.ClockView;
+import customview.lukfook.com.customview.views.EraserView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
 //    private DisInView mDisInView;
 //    private DisOutView mDisOutView;
 //    private ScreenView mScreenView;
-    private ClockView mClockView;
+//    private ClockView mClockView;
+    private EraserView mEraserView;
     //半径值
 //    private int radiu;
 
@@ -30,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
 //        mDisInView = (DisInView) findViewById(R.id.main_div);
 //        mDisOutView = (DisOutView) findViewById(R.id.main_dov);
 //        mScreenView = (ScreenView) findViewById(R.id.main_sv);
-        mClockView = (ClockView) findViewById(R.id.main_cvo);
-
+//        mClockView = (ClockView) findViewById(R.id.main_cvo);
+        mEraserView = (EraserView) findViewById(R.id.main_ev);
         /**
          * 开线程
          */
